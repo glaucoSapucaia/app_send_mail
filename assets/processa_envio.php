100e4e2f952f0c13bb51efe0a74e5fbb562db337
glaucoSapucaia <glaucopco@gmail.com>
2024-06-06 11:28:30 -0300

    class Mensagem | metodo mensagemValida() | Instancias de mensagens

--- assets/processa_envio.php
+++ assets/processa_envio.php
@@ -2,4 +2,48 @@
 // super global $_POST
 // Armazena dados enviados pelo usuário atravé sde formulários
     // print_r($_POST);
+
+    // Objeto EMAIL
+    class Mensagem {
+        // attrs
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+
+        // getters | setters
+        public function __get($attr) {
+            return $this->$attr;
+        }
+
+        public function __set($attr, $valor) {
+            $this->$attr = $valor;
+        }
+
+        // metodos
+        public function mensagemValida() {
+            // validando emails
+            if(empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    // instancias
+    $msg = new Mensagem();
+
+    // aplicando valores
+    $msg->__set('para', $_POST['para']);
+    $msg->__set('assunto', $_POST['assunto']);
+    $msg->__set('mensagem', $_POST['mensagem']);
+
+    // Valdinado mensagens
+    if($msg->mensagemValida()) {
+        echo 'Mensagem Válida';
+    } else {
+        echo 'Mensagem Inválida';
+    }
+
+    // testes
+    // print_r($msg);
 ?>
